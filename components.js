1bcb60384be07d595e0e2323ab654a85a20e9a3b
mabalde <lamaranabalde812@gmail.com>
2023-11-28 20:34:43 +0000

    debut

--- components.js
+++
@@ -1,7 +0,0 @@
-/* ===== DES IMAGES */
-const LEVEL_IMG = new Image();
-LEVEL_IMG.src = 'img/level.png';
-const LIFE_IMG = new Image();
-LIFE_IMG.src = 'img/life.png';
-const SCORE_IMG = new Image();
-SCORE_IMG.src = 'img/score.png';
